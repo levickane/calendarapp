17d88f4584740bb74feb3c7ef487720875edf122
Levi Kane <oneleggedlevi09@gmail.com>
2021-04-08 20:10:46 -0500

    displaying colors properly. working on capturing the data from the text area

--- script.js
+++ script.js
@@ -2,14 +2,38 @@ var currentDay = $('#currentDay')
 currentDay.text(moment().format('dddd, MMMM Do'))
 
 var timeBlockContainer = $('.container')
+var currHour = moment().format('HH')
 
-for (let i = 0; i < 9; i++) {
+for (var i = 9; i < 19; i++) {
   var timeBlockDiv = $(`<div class="time-block row">
-    <div class="hour col-2">Time</div>
-    <textarea class="description col-8">text area</textarea>
-    <div class="saveBtn col-2"><i style='font-size:24px' class='fas'>&#xf0c7;</i></div>
-    </div>`)
+    <div class="hour col-1">${i}</div>
+    <textarea class="description col-10" id="${i}"></textarea>
+    <button class="saveBtn col-1" id='saveBtn${i}' type="submit"><i style='font-size:24px' class='fas'>&#xf0c7;</i></div>
+    </button>`)
   timeBlockContainer.append(timeBlockDiv)
-  var hourDiv = $('.hour')
-  hourDiv.text(moment().format('LT'))
+}
+
+for (var i = 9; i < 19; i++) {
+  var textAreaEl = $(`#${i}`)
+  if (currHour == textAreaEl.attr('id')) {
+    textAreaEl.addClass('present')
+  }
+}
+
+for (var i = 9; i < 19; i++) {
+  var textAreaEl = $(`#${i}`)
+  if (textAreaEl.attr('id') < currHour) {
+    textAreaEl.addClass('past')
+  }
+}
+
+for (var i = 9; i < 19; i++) {
+  var textAreaEl = $(`#${i}`)
+  if (textAreaEl.attr('id') > currHour) {
+    textAreaEl.addClass('future')
+  }
+}
+
+for (var i = 9; i < 19; i++) {
+  var saveBtn = $(`#saveBtn${i}`)
 }
